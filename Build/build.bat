9c2acb76ca6c8eca96922b39e1ff74a86fbee374
eNuoTiZ <38533607+eNuoTiZ@users.noreply.github.com>
2021-04-05 23:06:01 -0400

    Fix case

--- Build/build.bat
+++ Build/build.bat
@@ -2,7 +2,7 @@ echo Start building...
 
 "C:\Program Files\Unity\Hub\Editor\2019.4.18f1\Editor\Unity.exe" -quit -batchmode -projectPath . -logFile ./Logs/Builds/build.log ./Minesweeper.apk
 
-type ./Logs/Builds/build.log
+TYPE ./Logs/Builds/build.log
 
 rem -executeMethod BuildMyGame.BuildAndroid
 echo Build ended...
