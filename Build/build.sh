82ed2fbfb685a00ea2317b6edd357b6b9819a15a
OlivierEdoki <olivier@edokiacademy.com>
2021-04-07 22:20:20 -0400

    keep log outside drone temp folder

--- Build/build.sh
+++ Build/build.sh
@@ -2,9 +2,21 @@
 
 echo Start building...
 
-ls -lh
+outputDir=backup_dir=$(date +'%m%d%Y-%H%M%S')
 
-'/Applications/Unity/Hub/Editor/2019.4.18f1/Unity.app/Contents/MacOS/Unity' -quit -batchmode -projectPath . -executeMethod BuildMyGame.BuildIos -logFile ./Logs/Builds/build.log
+mkdir /Documents/BuildLogs/$outputDir/
 
+'/Applications/Unity/Hub/Editor/2019.4.18f1/Unity.app/Contents/MacOS/Unity' -quit -batchmode -projectPath . -executeMethod BuildMyGame.BuildIos -logFile /Documents/BuildLogs/$outputDir/build.log
+
+exitCode=$?
+
+if [ $exitCode -eq 0 ]
+then
+  echo "Build succeeded!"
+  exit 0
+else
+  echo "Build failed, check log for more information." >&2
+  exit $exitCode
+fi
 
 echo Build ended...
