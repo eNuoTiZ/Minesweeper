f5a1a44c97472cb52ef61f39b52a4da9cf331ada
eNuoTiZ <38533607+eNuoTiZ@users.noreply.github.com>
2021-04-07 21:32:20 -0400

    debug permission denied

--- Build/build.sh
+++ Build/build.sh
@@ -2,6 +2,8 @@
 
 echo Start building...
 
+ls -lh
+
 '/Applications/Unity/Hub/Editor/2019.4.18f1/Unity.app/Contents/MacOS/Unity' -quit -batchmode -projectPath . -executeMethod BuildMyGame.BuildIos -logFile ./Logs/Builds/build.log
 
 
